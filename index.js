2dee4f86f985cacc4f08c07e852c5b931657cac2
Arjun Yadav <arjunyadav001@gmail.com>
2019-05-31 00:44:21 +0530

    Slight Changes

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const Express = require("express")
 const App = Express()
 const {router} = require("./routes/user")
 const DB = require("./models/db").DB
+
 App.use(Express.json())
 App.use(Express.urlencoded({extended: true}))
+App.use("/",Express.static(__dirname + "/public"))
 
 App.use("/user",router)
 
--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,56 @@
 const router = require("express").Router()
 const { DB,student,subject } = require("../models/db")
 
-router.get("/" ,(req,res) => {
-    console.log(student.findAll().then((result) => {
+router.get("/" , (req,res) => {
+    student.findAll().then((result) => {
+        res.status(201).send(result)
         console.log(result)
-    }))
-    res.send("Hello")
+    }).catch((err) => {
+        res.status(400).send(err)
+        console.log(err)
+    })
 })
 
-router.post("/" ,(req,res) => {
+router.get("/sub" ,(req,res) => {
+    subject.findAll().then((result) => {
+        res.status(201).send(result)
+        console.log(result)
+    }).catch((err) => {
+        res.status(400).send(err)
+        console.log(err)
+    })
+})
+
+router.post("/sub" ,(req,res) => {
     try {
-        res.status(201).send(student.create({
-           Roll: req.body.roll,
-           Name: req.body.name 
-        }))
+        subject.create({
+            English: req.body.eng,
+            Hindi: req.body.hin,
+            Maths: req.body.mat
+        }).then((result) => {
+            res.status(201).send(result)
+            console.log(result)
+        })
     }
     catch(err) {
-        res.status(400).send({
-            Error: err
+        res.status(400).send(err)
+        console.log(err)
+    }
+})
+
+router.post("/", (req,res) => {
+    try {
+        student.create({
+            Roll: req.body.roll,
+            Name: req.body.name
+        }).then((result) => {
+            res.status(201).send(result)
+            console.log(result)
         })
+    }
+    catch(err) {
+        res.status(400).send(err)
+        console.log(err)
     }
 })
 
